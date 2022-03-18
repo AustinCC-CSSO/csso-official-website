65984e698a410f9b66d08e91596f22b07517bd9c
malikrawashdeh <malikrawashdeh5@gmail.com>
2022-03-18 09:47:40 -0500

    prof pics

--- src/components/Profiles/Data.js
+++ src/components/Profiles/Data.js
@@ -5,27 +5,27 @@ export const profilesData = [
     position: "President",
   },
   {
-    profPic: require("./ProfileImages/nathanPfp"),
+    profPic: require("./ProfileImages/malik.jpeg"),
     fullName: "Malik Rawashdeh",
     position: "Vice President",
   },
   {
-    profPic: require("./ProfileImages/nathanPfp"),
+    profPic: require("./ProfileImages/profpic.png"),
     fullName: "Alya Mansoor",
     position: "Design Manager",
   },
   {
-    profPic: require("./ProfileImages/nathanPfp"),
+    profPic: require("./ProfileImages/profpic.png"),
     fullName: "Litzy Carela",
     position: "Historian",
   },
   {
-    profPic: require("./ProfileImages/nathanPfp"),
+    profPic: require("./ProfileImages/profpic.png"),
     fullName: "Jason Kim",
     position: "Treasurer",
   },
   {
-    profPic: require("./ProfileImages/nathanPfp"),
+    profPic: require("./ProfileImages/profpic.png"),
     fullName: "Daniel Hamdan",
     position: "Secretary",
   },
